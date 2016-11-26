b504b67d0a0491f741f1e33b340cecdb24d6b1cb
Sidney Dodson <siddodson@gmail.com>
2016-11-26 13:36:29 -1000

    Completed

--- eu01.js
+++ eu01.js
@@ -1,11 +1,11 @@
 function multiplesFinder(para){
   var numHolder =0;
   for ( var i = 0; i < para; i++){
-    if(i%3===0){
+    if(i%3===0||i%5===0){
       numHolder += i;
     }
   }
   console.log(numHolder);
 }
 
-multiplesFinder(10);
+multiplesFinder(1000);
