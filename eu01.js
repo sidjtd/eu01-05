bcc1cac1da70939e59f0d37598719f61147af892
Sidney Dodson <siddodson@gmail.com>
2016-11-26 14:01:01 -1000

    Euler 2 Progress

--- eu01.js
+++ eu01.js
@@ -1,4 +1,4 @@
-function multiplesFinder(para){
+/*function multiplesFinder(para){
   var numHolder =0;
   for ( var i = 0; i < para; i++){
     if(i%3===0||i%5===0){
@@ -7,4 +7,25 @@ function multiplesFinder(para){
   }
   console.log(numHolder);
 }
-multiplesFinder(1000);
+multiplesFinder(1000);*/
+//Answer: 233168
+
+function fibonacciSequence(para){
+  var fibA = 1;
+  var fibB = 2;
+  var fibSto = 0;
+  var fibSum = 0;
+
+  for ( var i = 0; i < para; i++){
+    // console.log(fibA);
+    fibSto = fibB;-
+    fibB = fibB + fibA;
+    fibA = fibSto;
+    // console.log(fibB);
+    console.log(fibA+" + "+fibB +" = " + (fibA+fibB));
+
+    }
+
+}
+
+fibonacciSequence(10);
