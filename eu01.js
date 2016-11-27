e58bb606a514037c10db711a7aff8d200c041259
Sidney Dodson <siddodson@gmail.com>
2016-11-26 15:44:46 -1000

    Euler 3 Solved

--- eu01.js
+++ eu01.js
@@ -1,4 +1,12 @@
 debugger;
+//Helper Functions
+function isDivisibleBy(numA,numB){
+  if (numA % numB === 0){
+    return true;
+  }else{
+    return false;
+  }
+}
 /*function multiplesFinder(para){
   var numHolder =0;
   for ( var i = 0; i < para; i++){
@@ -11,7 +19,7 @@ debugger;
 multiplesFinder(1000);*/
 //Answer: 233168
 
-function fibonacciSequence(para){
+/*function fibonacciSequence(para){
   var fibA = 1;
   var fibB = 2;
   var fibSto = 0;
@@ -27,4 +35,19 @@ function fibonacciSequence(para){
     }
   }
 }
-fibonacciSequence(10000);
+fibonacciSequence(10000);*/
+//Answer: 4613732
+
+function largestPrimeFactor(para){
+  var factors = [];
+  for(var i = 2; i <= para; i++){
+    while (isDivisibleBy(para,i)){
+      factors.push(i);
+      para = para / i;
+    }
+  }
+  return factors;
+}
+// console.log(largestPrimeFactor(13195));
+console.log(largestPrimeFactor(600851475143));
+//Answer: 6857
